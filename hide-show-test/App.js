632fcdbc6e1a89a8f7ab55ef01f68dc12ab7e136
Student-AY <Student-Ahmet-Yilmaz@hotmail.com>
2022-02-17 13:03:37 +0100

    init

--- hide-show-test/App.js
+++ hide-show-test/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import{ SafeAreaView,StyleSheet,View,Button,Image} from 'react-native';
 const App = () => {
   const [show,setShow] = useState(true);
+  const [show2,setShow2] = useState(true);
 
   return(
     <SafeAreaView style={{flex:1}}>
@@ -19,6 +20,23 @@ const App = () => {
           title="Hide/Show React(Logo)"
           onPress={() => setShow(!show)}
           />
+      </View>
+
+      <View style={styles.container}>
+        {
+          show2 ? (
+              <Image
+              source={{uri:"https://toppng.com/uploads/preview/png-effects-11552159189h8htyll5hu.png"}}
+              style={{width:250,height:250,marginBottom:10}}
+              />
+          ) : null
+        }
+        <Button 
+        title="Hide/Show Skull(picture)"
+        onPress={() => setShow2(!show2)}
+        />
+
+
       </View>
   </SafeAreaView>
   )
